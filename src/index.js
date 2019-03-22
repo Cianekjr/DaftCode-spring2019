a94f8111155d2914dad6c782c2df1b91633c2390
Mateusz Chrzanowski <Cianekjr@gmail.com>
2019-03-22 14:54:31 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles/style.css";
+
+import script from "./script";
+script();
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  mode: "production", // development/production version
+  watch: true, // watch for changes in any of the resolved files
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
